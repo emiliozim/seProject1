6d3fa52c6539fca6c7c25edbfbffcb4230dd12c1
emiliozim <43814391+emiliozim@users.noreply.github.com>
2019-09-24 13:37:29 +0200

    adden person, arrangement og deltager

---
+++ src/softwareengineering/project/model/Person.java
@@ -0,0 +1,16 @@
+package softwareengineering.project.model;
+
+public class Person {
+
+    private String fornavn;
+    private String etternavn;
+    private int fodselnummer;
+
+    public Person(String fornavn, String etternavn, int fodselnummer) {
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.fodselnummer = fodselnummer;
+    }
+
+
+}
