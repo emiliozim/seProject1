2a5593a099d85224c0ed04737c222343454c55fe
magnusnorvik <43719819+magnusnorvik@users.noreply.github.com>
2019-09-25 11:27:53 +0200

    LagetTest

--- test/softwareengineering/project/model/PersonTest.java
+++ test/softwareengineering/project/model/PersonTest.java
@@ -1,4 +1,19 @@
 package softwareengineering.project.model;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PersonTest {
+
+    @Test
+    public Person testAddPerson() {
+        return null;
+    }
+
 }
+
+
