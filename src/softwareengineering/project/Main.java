b39d4a34e24930140ceefed14f8bcd801746a5c0
emiliozim <43814391+emiliozim@users.noreply.github.com>
2019-09-25 10:27:39 +0200

    adden person, arrangement og deltager

--- src/softwareengineering/project/Main.java
+++ src/softwareengineering/project/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     }
     public static void main(String[] args) {
-        System.out.println("hei world!");
+        System.out.println("hei wornei!");
         launch(args);
     }
 }
