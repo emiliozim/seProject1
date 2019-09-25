ac5f7eec66fcc65e9295a182c5dfb5e9a0f1ac73
magnusnorvik <43719819+magnusnorvik@users.noreply.github.com>
2019-09-25 11:06:20 +0200

    sdfsdf

--- src/softwareengineering/project/controller/MainWindowController.java
+++ src/softwareengineering/project/controller/MainWindowController.java
@@ -6,9 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.paint.Paint;
 import softwareengineering.project.model.Organization;
-import softwareengineering.project.model.User;
 
 public class MainWindowController {
 
@@ -33,15 +31,15 @@ public class MainWindowController {
 
                 //Skulle ikke legge så mye tid på innlogg, men dette føles ganska simpelt.
 
-                User sessionUser = createUserSession();
+                //User sessionUser = createUserSession();
 
-                message.setText("Innlogget som " + sessionUser.getUserName() + " fra " + sessionUser.getOrganization().getOrgName());
+                //message.setText("Innlogget som " + sessionUser.getUserName() + " fra " + sessionUser.getOrganization().getOrgName());
 
             }
         });
 
     }
-
+/*
     private User createUserSession() {
 
         if(loginSuccessful()) {
@@ -64,6 +62,6 @@ public class MainWindowController {
         return true;
 
     }
-
+*/
 
 }
